1af6b62e740b3f67f073a4258bc87b8a922ec374
Artur Siedlerski <don.siedler@gmail.com>
2023-02-14 21:59:42 +0100

    Add class init; add method to find the cheapest flight

--- flight_data.py
+++ flight_data.py
@@ -1,2 +1,38 @@
+from datetime import datetime
+from pprint import pprint
+import requests
+
+from flight_search import KIWI_ENDPOINT, API_HEADERS
+
+
 class FlightData:
-    pass
+    def __init__(self, city_code):
+        self.fly_from = "LON"
+        self.fly_to = city_code,
+        self.date_from = "15/02/2023"  # TODO 1: pass in tomorrow's date
+        self.date_to = "08/03/2023"  # TODO 2: calculate the end date
+        self.nights_in_dst_from = 7
+        self.nights_in_dst_to = 21
+        self.ret_from_diff_city = False
+        self.ret_to_diff_city = False
+        self.curr = "GBP"
+        self.limit = 1
+
+    def get_cheapest_flight(self):
+        params = {
+            "fly_from": self.fly_from,
+            "fly_to": self.fly_to,
+            "date_from": self.date_from,
+            "date_to": self.date_to,
+            "nights_in_dst_from": self.nights_in_dst_from,
+            "nights_in_dst_to": self.nights_in_dst_to,
+            "ret_from_diff_city": self.ret_from_diff_city,
+            "ret_to_diff_city": self.ret_to_diff_city,
+            "curr": self.curr,
+            "limit": self.limit,
+        }
+
+        response = requests.get(f"{KIWI_ENDPOINT}/v2/search", headers=API_HEADERS, params=params)
+        response.raise_for_status()
+        data = response.json()
+        pprint(data)
