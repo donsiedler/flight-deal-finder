98f87eb6587cd2a5984a9a068b3f298d5d06eed1
Artur Siedlerski <don.siedler@gmail.com>
2023-02-14 19:58:44 +0100

    Setup KIWI API; add city code searching logic

--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,22 @@
+import os
+import requests
+
+KIWI_ENDPOINT = "https://api.tequila.kiwi.com/locations/query"
+API_HEADERS = {
+    "apikey": os.environ.get("KIWI_API_KEY"),
+    "content-type": "application/json",
+}
+
+
 class FlightSearch:
+
     def get_destination_code(self, city_name):
-        return "TESTING"
+        params = {
+            "term": city_name,
+            "location_types": "city"
+        }
+        response = requests.get(KIWI_ENDPOINT, headers=API_HEADERS, params=params)
+        response.raise_for_status()
+        data = response.json()
+        city_code = data["locations"][0]["code"]
+        return city_code
