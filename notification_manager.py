a18170f57ee93f5fb13d3a5082c49fc343ac57cd
Artur Siedlerski <don.siedler@gmail.com>
2023-02-15 20:27:50 +0100

    code cleanup

--- notification_manager.py
+++ notification_manager.py
@@ -1,22 +1,20 @@
 import os
 from twilio.rest import Client
 
+TWILIO_ACCOUNT_SID = os.environ.get("TWILIO_ACCOUNT_SID")
+TWILIO_AUTH_TOKEN = os.environ.get("TWILIO_AUTH_TOKEN")
+TWILIO_NUMBER = os.environ.get("TWILIO_NUMBER")
+MY_NUMBER = os.environ.get("MY_NUMBER")
+
 
 class NotificationManager:
-    example_message = "Low price alert! Only 63 GBP to fly from London-STN to Berlin-SFX, from 2023-02-15 to " \
-                      "2023-02-28."
+    def __init__(self):
+        self.client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
-    account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
-    auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
-    twilio_number = os.environ.get("TWILIO_NUMBER")
-    my_number = os.environ.get("MY_NUMBER")
-
-    client = Client(account_sid, auth_token)
-
-    message = client.messages.create(
-        from_=f"whatsapp:+{twilio_number}",
-        body=f"{example_message}",
-        to=f"whatsapp:+{my_number}"
-    )
-
-    print(message.sid)
+    def send_notification(self, message):
+        message = self.client.messages.create(
+            from_=f"whatsapp:+{TWILIO_NUMBER}",
+            body=f"{message}",
+            to=f"whatsapp:+{MY_NUMBER}"
+        )
+        print(message.sid)
