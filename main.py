b62a6babee3d784cabc76c6abaf7a94b8daf9e41
Artur Siedlerski <don.siedler@gmail.com>
2023-02-15 20:45:29 +0100

    Add price comparison logic; send notifications

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from data_manager import DataManager
 from flight_search import FlightSearch
 from flight_data import FlightData
+from notification_manager import NotificationManager
 
 data_manager = DataManager()
 sheet_data = data_manager.get_destination_data()
@@ -13,6 +14,16 @@ for row in sheet_data:
 data_manager.destination_data = sheet_data
 data_manager.update_destination_codes()
 
-for row in sheet_data:
-    flight_data = FlightData(row["iataCode"])
-    flight_data.get_cheapest_flight()
+notification_manager = NotificationManager()
+
+for city in sheet_data:
+    flight_data = FlightData(city["iataCode"])
+    flight = flight_data.get_cheapest_flight()
+
+    if flight.price <= city["lowestPrice"]:
+        notification_manager.send_notification(
+            message=f"Low price alert! Only £{flight.price} to fly from "
+                    f"{flight.origin_city}-{flight.origin_airport} to "
+                    f"{flight.destination_city}-{flight.destination_airport}, "
+                    f"from {flight.out_date} to {flight.return_date}."
+        )
