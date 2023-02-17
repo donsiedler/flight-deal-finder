c692d2d2b7476ec2c81c1327e857e13260c75e50
Artur Siedlerski <don.siedler@gmail.com>
2023-02-17 10:03:34 +0100

    Add error handling for empty flight obj

--- main.py
+++ main.py
@@ -20,10 +20,13 @@ for city in sheet_data:
     flight_data = FlightData(city["iataCode"])
     flight = flight_data.get_cheapest_flight()
 
-    if flight.price <= city["lowestPrice"]:
-        notification_manager.send_notification(
-            message=f"Low price alert! Only £{flight.price} to fly from "
-                    f"{flight.origin_city}-{flight.origin_airport} to "
-                    f"{flight.destination_city}-{flight.destination_airport}, "
-                    f"from {flight.out_date} to {flight.return_date}."
-        )
+    try:
+        if flight.price <= city["lowestPrice"]:
+            notification_manager.send_notification(
+                message=f"Low price alert! Only £{flight.price} to fly from "
+                        f"{flight.origin_city}-{flight.origin_airport} to "
+                        f"{flight.destination_city}-{flight.destination_airport}, "
+                        f"from {flight.out_date} to {flight.return_date}."
+            )
+    except AttributeError:
+        pass
